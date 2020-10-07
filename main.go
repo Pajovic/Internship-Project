b6ab4e8b13c8e70e8cdd26886afee636f1741fdd
zukovicfilip <zukovicfilip@gmail.com>
2020-10-07 12:04:51 +0200

    Implemented CRUD for products using REST API

--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"internship_project/controllers"
+	"internship_project/repositories"
+	"internship_project/services"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/lytics/confl"
 )
 
+type config struct {
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	DatabaseURL string `json:"database_url"`
+}
+
 func main() {
+	productController, connection := getController()
+	defer connection.Close()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler)
-	http.Handle("/", r)
+
+	productRouter := r.PathPrefix("/product").Subrouter()
+
+	productRouter.HandleFunc("", productController.GetAllProducts).Methods("GET")
+
+	productRouter.HandleFunc("/{id}", productController.GetProductById).Methods("GET")
+
+	productRouter.HandleFunc("", productController.AddProduct).Methods("POST")
+
+	productRouter.HandleFunc("/{id}", productController.UpdateProduct).Methods("PUT")
+
+	productRouter.HandleFunc("/{id}", productController.DeleteProduct).Methods("DELETE")
+
 	http.ListenAndServe(":8000", r)
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello world from Gorilla Mux")
+func getController() (controllers.ProductController, *pgxpool.Pool) {
+	var conf config
+	if _, err := confl.DecodeFile("database.conf", &conf); err != nil {
+		panic(err)
+	}
+
+	poolConfig, _ := pgxpool.ParseConfig(conf.DatabaseURL)
+
+	connection, err := pgxpool.ConnectConfig(context.Background(), poolConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	productRepository := repositories.ProductRepository{DB: connection}
+	productService := services.ProductService{Repository: productRepository}
+	productController := controllers.ProductController{Service: productService}
+
+	return productController, connection
 }
---
+++ models/product.go
@@ -0,0 +1,9 @@
+package models
+
+type Product struct {
+	ID       string
+	Name     string
+	Price    float32
+	Quantity int32
+	IDC      string
+}
